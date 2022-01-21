502f28cbde980e8192d17c684fa886ebae96de6a
gvardovski <95145252+gvardovski@users.noreply.github.com>
2022-01-22 01:25:03 +0300

    Add files via upload
    Block four JS files

---
+++ 4-3.js
@@ -0,0 +1,11 @@
+let n1 = "Генадий";
+let n = "Станислав";
+function cheсkn(a,b){
+    if ( b == a ){
+        console.log("Привет "+b);
+    }
+    else{
+        console.log("Нет такого имени");
+    }
+}
+cheсkn(n,n1);
