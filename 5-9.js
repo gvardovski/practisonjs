73b5011ef4a761cc8543c3dc31184b95803c84e4
gvardovski <95145252+gvardovski@users.noreply.github.com>
2022-01-27 01:26:31 +0300

    Delete 5-9.js

--- 5-9.js
+++
@@ -1,9 +0,0 @@
-let x = 40;
-step:
-    for (let i = 2; i <= x; i++) {
-        for (let j = 2; j < i; j++) {
-            if (i % j == 0) continue step;
-        }
-        console.log(i);
-    }
-
