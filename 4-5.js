04cf689c4c7e86e4e17828ebe13cbce6d48a4bd9
gvardovski <95145252+gvardovski@users.noreply.github.com>
2022-01-27 01:27:38 +0300

    Delete 4-5.js

--- 4-5.js
+++
@@ -1,9 +0,0 @@
-let x = 5;
-function checkprost (x){
-    let c=0;
-    for (let i=2;i<x;i++){
-        if (x % i == 0) {c++;}
-    }
-    if ( c==0 ) {console.log("Простое число");}
-}
-checkprost(x);
