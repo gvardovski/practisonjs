a4922b5792e4834e9ca783e3ddb81ac0fd4acfeb
gvardovski <95145252+gvardovski@users.noreply.github.com>
2022-02-02 01:49:24 +0300

    Add files via upload

--- 5-3.js
+++ 5-3.js
@@ -1,6 +1,6 @@
 let arr = ["Stas","Tom","John"];
 for (let i=0;i<arr.length;i++) {
-    arr[i]+=" Hello";
+    arr[i]="Hello "+arr[i];
 }
 console.log(arr);
 
--- 5-7.js
+++ 5-7.js
@@ -1,6 +1,6 @@
-let arr = [1,6,7,8,3,4,5,6] ;
-function findind(arr,x){
-    return arr.find(item=>item==x);
+const arr = [1,6,7,8,3,4,5,6] ;
+function SearchId(arr,x){
+    return arr.findIndex(item=>item==x);
 }
-console.log(findind(arr,4));
+console.log(SearchId(arr,3));
 
